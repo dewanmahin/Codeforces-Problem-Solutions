b5d0742c15694017ed5253af57e345ba470924e5
DEWAN <dewanmahinchy@gmail.com>
2023-06-07 00:11:37 +0600

    boy_or_girl.cpp added

---
+++ boy_or_girl.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string name;
+    cin>>name;
+
+    if(name.size()%2!=0){
+        cout<<"IGNORE HIM!"<<endl;
+    }else{
+        cout<<"CHAT WITH HER!"<<endl;
+    }
+
+    return 0;
+}
