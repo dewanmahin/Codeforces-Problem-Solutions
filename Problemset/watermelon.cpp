cde6b9aeaec9750b9dc325b72adb793abc1b6d0f
DEWAN <dewanmahinchy@gmail.com>
2023-08-11 01:03:06 +0600

    added watermelon.cpp file

---
+++ Problemset/watermelon.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int w; cin>>w;
+    if(w%2 == 0 && w > 2){
+        printf("YES");
+    }else{
+        printf("NO");
+    }
+
+    return 0;
+}
+
+// Problem link: https://codeforces.com/group/MWSDmqGsZm/contest/223206/problem/B
